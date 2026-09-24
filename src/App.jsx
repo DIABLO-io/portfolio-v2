b5d45e58acdd58c8df2be19e7b62a7741862aa00
DIABLO <diablokar4@gmail.com>
2026-09-24 17:25:46 +0100

    Compose about and experience sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from './components/layout/Navbar'
 import Hero from './components/sections/Hero'
+import About from './components/sections/About'
+import Experience from './components/sections/Experience'
 
 function App() {
   return (
     <main className="min-h-screen bg-slate-950 text-white">
       <Navbar />
       <Hero />
+      <About />
+      <Experience />
     </main>
   )
 }
