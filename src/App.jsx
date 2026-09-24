056da0602783c7a7cb6c604cf52d2fe9fd53b8c9
DIABLO <diablokar4@gmail.com>
2026-09-24 17:56:08 +0100

    Add section reveal transitions

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,37 @@ import Projects from './components/sections/Projects'
 import Skills from './components/sections/Skills'
 import Education from './components/sections/Education'
 import Contact from './components/sections/Contact'
+import Reveal from './components/ui/Reveal'
 
 function App() {
   return (
     <main className="min-h-screen bg-slate-950 text-white">
       <Navbar />
       <Hero />
-      <About />
-      <Experience />
-      <Projects />
-      <Skills />
-      <Education />
-      <Contact />
+
+      <Reveal>
+        <About />
+      </Reveal>
+
+      <Reveal delay={40}>
+        <Experience />
+      </Reveal>
+
+      <Reveal delay={60}>
+        <Projects />
+      </Reveal>
+
+      <Reveal delay={60}>
+        <Skills />
+      </Reveal>
+
+      <Reveal delay={60}>
+        <Education />
+      </Reveal>
+
+      <Reveal delay={60}>
+        <Contact />
+      </Reveal>
     </main>
   )
 }
