3669c93eb40376b83bb3c14b78efa046937a3490
DIABLO <diablokar4@gmail.com>
2026-09-24 17:52:49 +0100

    Compose education and contact sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from './components/sections/About'
 import Experience from './components/sections/Experience'
 import Projects from './components/sections/Projects'
 import Skills from './components/sections/Skills'
+import Education from './components/sections/Education'
+import Contact from './components/sections/Contact'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Experience />
       <Projects />
       <Skills />
+      <Education />
+      <Contact />
     </main>
   )
 }
